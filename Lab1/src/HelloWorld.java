c198584e0f576b8c27257630aac75e0f09872ce0
MrNull <100611901+TheMrNull@users.noreply.github.com>
2026-03-10 11:44:32 +0100

    Lab01

---
+++ Lab1/src/HelloWorld.java
@@ -0,0 +1,17 @@
+import static java.lang.Math.sqrt;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hey," + " fuck off");
+        double x,y;
+        x = -1;
+        y = 2;
+        double result = x/y;
+        result = sqrt(result);
+        System.out.println("Result = " + result);
+    }
+
+
+
+}
